c50131a499d08e97548554a3b338a1f71b4ebd3f
Nuha Al-Sameai <noha.pepeep1@gmail.com>
2020-09-19 15:42:43 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route , NavLink } from 'react-router-dom';
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
+}
 const Home = () => {
   return (
     <div>
