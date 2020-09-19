1b423f83daffdfd0120d5e4868a26754137eff59
Nuha Al-Sameai <noha.pepeep1@gmail.com>
2020-09-19 15:47:25 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -10,33 +10,6 @@ const link = {
   color: 'white',
 }
 
-const Navbar = () =>
-  <div>
-    <NavLink
-      to="/"
-      exact
-      style={link}
-      activeStyle={{
-        background: 'darkblue'
-      }}
-    >Home</NavLink>
-    <NavLink
-      to="/about"
-      exact
-      style={link}
-      activeStyle={{
-        background: 'darkblue'
-      }}
-    >About</NavLink>
-    <NavLink
-      to="/login"
-      exact
-      style={link}
-      activeStyle={{
-        background: 'darkblue'
-      }}
-    >Login</NavLink>
-  </div>;
 
 const Home = () => {
   return (
